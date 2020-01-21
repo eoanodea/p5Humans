d902cabe76f2e9456bf9517fe88d083def7ff24b
Eoan O Dea <odeaeoan@gmail.com>
2020-01-21 10:41:03 +0000

    Created Male and Female classes that inherit from the Human class

---
+++ js/sketch.js
@@ -0,0 +1,64 @@
+let humans = [], humanKey = []
+const numOfHumans = 6
+let canvasWidth = 600, canvasHeight = 600, colWidth, rowHeight
+const minRadius = 20, maxRadius = 25, minVelocity = -4, maxVelocity = 4, minPulse = 5, maxPulse = 20
+
+/**
+ * setup():
+ * Sets the canvas width & height to that of the window.
+ * 
+ * Creates a canvas with that width and height.
+ * 
+ * Sets the background to black
+ * 
+ * Creates a number of Humans depending on the 
+ * numOfHumans variable
+ */
+function setup() {
+    canvasWidth = windowWidth
+    canvasHeight = windowHeight
+
+    createCanvas(canvasWidth, canvasHeight)
+    background(0)
+
+    for (let i = 0; i < numOfHumans; i++) {
+        if(i >= numOfHumans/2) humans.push(new Female(i));
+        else humans.push(new Male(i));
+    }
+}
+
+/**
+ * draw():
+ * Runs every frame
+ * 
+ * Sets the canvas background to black
+ * 
+ * Runs the drawGrid function
+ * 
+ * Runs the mapHumans function
+ */
+function draw() {
+    background(255)
+    drawGrid()
+    mapHumans()
+}
+
+function drawGrid() {
+    for(let x = 0; x < canvasWidth; x+=colWidth) {
+        for(let y = 0; y < canvasHeight; y+=rowHeight) {
+            strokeWeight(1)
+            line(x, 0, x, height);
+            line(0, y, width, y)
+        }
+    }
+}
+
+function mapHumans() {
+    humans.forEach(human => {
+
+        human.render();
+        human.checkEdges();
+        human.pulseHuman()
+        human.step();
+    });
+}
